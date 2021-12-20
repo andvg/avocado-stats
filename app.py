1757e9f1375608de97b7f76c648635deead85612
Andrea V <33574513+andvg@users.noreply.github.com>
2021-12-20 16:29:53 +0100

    add

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 import streamlit as st
 import requests
 
+st.set_page_config(
+     page_title="CoD Warzone stats - Avocado Team",
+     page_icon="🔫",
+     layout="centered",
+     initial_sidebar_state="auto",
+     menu_items={
+         'andreavaghi.it': 'https://andreavaghi.it'
+     }
+ )
+
 st.title('🥑 Avocado Team')
 st.write('CoD Warzone stats')
 
 sso_token = st.text_input('Enter sso_token')
 
-if st.button('Calc'):
+if st.button('Go!'):
     cookies = {'ACT_SSO_COOKIE': sso_token}
-    resp_profile = requests.get('https://my.callofduty.com/api/papi-client/crm/cod/v2/title/mw/platform/uno/uno/11633078933998920206/matches/wz/start/0/end/0/details', cookies=cookies)
-    uno = resp_profile.json()['data']['matches']
-    st.json(uno)
+    resp_profile = requests.get('https://my.callofduty.com/api/papi-client/stats/cod/v1/title/mw/platform/uno/uno/11633078933998920206/profile/type/wz', cookies=cookies)
+    data = resp_profile.json()['data']['lifetime']['all']['properties']
+    username = resp_profile.json()['data']['username']
+    st.write('%s stats'%username)
+    st.dataframe(data)
 else:
     st.write('No data')
