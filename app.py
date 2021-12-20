10b0596afa009fd3ef774c986cf39c2783ab6e4b
Andrea V <33574513+andvg@users.noreply.github.com>
2021-12-20 16:16:03 +0100

    add

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-import streamlit as st 
+import streamlit as st
+import requests
 
 st.title('🥑 Avocado Team')
 st.write('CoD Warzone stats')
+
+sso_token = st.text_input('Enter sso_token')
+
+cookies = {'ACT_SSO_COOKIE': sso_token}
+resp_profile = requests.get('https://my.callofduty.com/api/papi-client/crm/cod/v2/title/mw/platform/uno/uno/11633078933998920206/matches/wz/start/0/end/0/details', cookies=cookies)
+uno = resp_profile.json()['data']['matches']
+st.json(uno)
