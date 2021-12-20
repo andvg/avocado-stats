0ffe03d85eb40f4a0d631b10d8b0f92aa1aee288
Andrea V <33574513+andvg@users.noreply.github.com>
2021-12-20 16:21:25 +0100

    add

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ st.write('CoD Warzone stats')
 
 sso_token = st.text_input('Enter sso_token')
 
-def connect():
+if st.button('Calc'):
     cookies = {'ACT_SSO_COOKIE': sso_token}
     resp_profile = requests.get('https://my.callofduty.com/api/papi-client/crm/cod/v2/title/mw/platform/uno/uno/11633078933998920206/matches/wz/start/0/end/0/details', cookies=cookies)
     uno = resp_profile.json()['data']['matches']
     st.json(uno)
+else:
+    st.write('No data')
