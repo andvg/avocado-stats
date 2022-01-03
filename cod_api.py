568fbd22549eec4bb58accf7959b29cbb24cc298
Andrea V <33574513+andvg@users.noreply.github.com>
2022-01-03 01:05:29 +0100

    fix

--- cod_api.py
+++ cod_api.py
@@ -22,7 +22,8 @@ def get_friends_info():
 
 st.header('CoD Warzone stats')
 version = st.selectbox('API version', ['v1', 'v2'])
-if game = st.selectbox('Game', {'Modern Warfare':'mw', 'WWII':'wwii', 'Black Ops 4':'boa'})
+game = st.selectbox('Game', {'Modern Warfare':'mw', 'WWII':'wwii', 'Black Ops 4':'boa'})
+if game:
     st.write(game)
 
 sso_token = st.text_input('Enter sso_token')
